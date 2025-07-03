0e5ba134ec50e0e9dcd6b84d1ca8dbd0c51a3f6d
abhyarastogi <abhyarastogi@gmail.com>
2025-07-03 13:53:55 +0530

    bubble sort added

---
+++ week2_python/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubble_sort(array, len_of_array):
+    for i in range(len_of_array - 1) :
+        for j in range()
+
+array = list(map(int, input('Enter the elements of the array :').split()))
+array.sort()
+num_of_elements = len(array)
+bubble_sort(array)
