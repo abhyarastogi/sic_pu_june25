383c9aebc3ec07724d1454046a0d655124739317
abhyarastogi <abhyarastogi@gmail.com>
2025-07-08 20:08:38 +0530

    employee crud operatiions

--- week2_python/employees_crud_mysql.py
+++ week2_python/employees_crud_mysql.py
@@ -1,10 +1,9 @@
 import pymysql
 
-
 def connect_db():
     connection = None
     try:
-        connection = pymysql.Connect(host='localhost', user="root", password="root", database='abhya_db', port=3306, charset="utf8")
+        connection = pymysql.connect(host='localhost', user="root", password="root", database='abhya_db', port=3306, charset="utf8")
         print('Database Connected')
     except:
         print('Database Connection Failed')
