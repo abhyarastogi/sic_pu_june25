e09fa5713512bf47bb79fff89b034b2bf8cee424
abhyarastogi <abhyarastogi@gmail.com>
2025-07-09 22:40:40 +0530

    Implementation for queue added

---
+++ week1_python_basics/queue1.py
@@ -0,0 +1,38 @@
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def is_empty(self):
+        return None if len(self.queue) < 1 else False
+    
+    def enqueue(self):
+        item = input('Enter the element to add :')
+        self.queue.append(item)
+        print(f'{item} was added successfully')
+
+    def dequeue(self):
+        if self.is_empty() == False:
+            print(self.queue.pop(0))
+        else:
+            print('Queue is Empty')
+
+    def display(self):
+        if self.is_empty() == False:
+            print(self.queue)
+        else:
+            print('Queue is Empty')
+
+queue = Queue()
+while True:
+    print('1.Enqueue\n2.Dequeue\n3.Display\n4.Exit')
+    choice = int(input('Enter your Choice: '))
+    if choice == 4:
+        break
+    match(choice):
+        case 1:
+            queue.enqueue()
+        case 2:
+            queue.dequeue()
+        case 3:
+            queue.display()
